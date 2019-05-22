e8ba8f8a4b4645ec60ca28b19572a804c5ffd253
WeAreHer3 <50907153+WeAreHer3@users.noreply.github.com>
2019-05-22 17:20:03 +0700

    Update Spam.sh

--- Spam.sh
+++ Spam.sh
@@ -1,6 +1,27 @@
 !/bin/bash
-====================================================
 figlet -f standard DEFAULT
+Spam () {
+clear
+sleep 0.03
+echo "T"
+sleep 0.03
+echo "U"
+sleep 0.03
+echo "N"
+sleep 0.03
+echo "G"
+sleep 0.03
+echo "G"
+sleep 0.03
+echo "U"
+sleep 2
+echo " ============================= "
+sleep 2
+echo " ==        Spam Jink        == "
+sleep 0.7
+echo " ============================= "
+load(){
+
 
 
 
