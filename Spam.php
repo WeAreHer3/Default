dd2e32833bbdf228ef0b3b624e577e003483ea6f
WeAreHer3 <50907153+WeAreHer3@users.noreply.github.com>
2019-05-23 13:10:52 +0700

    Update Spam.php

--- Spam.php
+++ Spam.php
@@ -1,20 +1,5 @@
 <?php
-figlet -f standard DEFAULT
-Spam () {
-clear
-sleep 0.03
-echo "T"
-sleep 0.03
-echo "U"
-sleep 0.03
-echo "N"
-sleep 0.03
-echo "G"
-sleep 0.03
-echo "G"
-sleep 0.03
-echo "U"
-sleep 2
+
 echo " ============================= "
 sleep 2
 echo " ==        Spam Jink        == "
